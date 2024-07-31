47efbe1df15b8a2aefd680201b2a4377ceab2347
Shashank <shashankmende88@gmail.com>
2024-07-31 20:29:37 +0530

    full_stack_project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NewEnrollment from './components/NewEnrollment';
+import Patients from './components/Patients';
+import {useEffect} from 'react'
+
+function App() {
+
+  useEffect(()=>{
+    document.title = 'Orbysol Hospital Application'
+  })
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<NewEnrollment />} />
+        <Route path='/patients' element={<Patients/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
